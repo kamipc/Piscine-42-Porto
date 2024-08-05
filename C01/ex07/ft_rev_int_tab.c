679b8e22ba40ea7ec2a80bec9db2a2d468dc2faf
Camila P. Costa <126345439+kamipc@users.noreply.github.com>
2024-08-05 21:52:17 +0100

    Update ft_rev_int_tab.c
    fixing the code to finally work ahhhhhhhhhhhhh

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -16,22 +16,19 @@
 void	ft_rev_int_tab(int *tab, int size)
 {
 	int save_n;
-	int *ptr1;
-	int *ptr2;
 	int i;
 	int j;
 
 	i = 0;
 	j = size - 1;
-	ptr1 = &tab[i];
-	ptr2 = &tab[j];
-	while (i < size)
-	{
-		save_n = ptr1[i];
-		ptr1[i] = ptr2[j];
-		ptr2[j] = save_n;
+	while (i < j)
+	{   
+		save_n = tab[i];
+		tab[i] = tab[j];
+		tab[j] = save_n;
 		i++;
 		j--;
+
 	}
 }
 
@@ -39,15 +36,14 @@ int main()
 {
 	int numbers[] = {10, 5, 75, 220, 2};
 	int *ptrnbr = &numbers[0];
-	int	i;
-	for (i = 0; i < 5; i++)
+	for (int i = 0; i < 5; i++)
 	{
 		printf("%d, ", numbers[i]);
 	}
 	printf("\n");
 
 	ft_rev_int_tab(ptrnbr, 5);
-	for (i = 0; i < 5; i++)
+	for (int i = 0; i < 5; i++)
 	{
 		printf("%d, ", numbers[i]);
 	}
