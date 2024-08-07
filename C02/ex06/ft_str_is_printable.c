8a100a33c0009696b61150898dec93a7c92d730e
Camila P. Costa <126345439+kamipc@users.noreply.github.com>
2024-08-07 13:26:19 +0100

    ft_str_is_printable.c
    fixing issues with the code, it was doing the opposite of what was asked.

--- C02/ex06/ft_str_is_printable.c
+++ C02/ex06/ft_str_is_printable.c
@@ -16,22 +16,22 @@ int	ft_str_is_printable(char *str)
 	int	i;
 
 	i = 0;
-	if (str[0] == '\0')
+	if (!str[0])
 		return (1);
 	while (str[i] != '\0')
 	{
 		if (str[i] > 31 && str[i] < 127)
 			i++;
 		else
-			return (1);
+			return (0);
 	}
-	return (0);
+	return (1);
 }
 
 /*int main()
 {
-    char str[] = "\0\n%d%s";
-    char str2[] = "HELLOWORLD";
+    char str[] = "\n\0%d%s";
+    char str2[] = "HELLO WORLD";
     char str3[] = "H3ll0 W0rld!";
     char str4[] = "1234567";
     char str5[] = "";
