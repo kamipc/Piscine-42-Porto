f9bd2a333c3df29d751e715dac151c3221c2154a
Camila P. Costa <126345439+kamipc@users.noreply.github.com>
2024-08-14 10:08:34 +0100

    add ex05
    still not fully functional

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -10,14 +10,43 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
-#include <string.h>
+#include <bsd/string.h>
 
-unsigned int    ft_strlcat(char *dest, char *src, unsigned int size)
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
+	unsigned int	i;
+	unsigned int	j;
 
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[s] != '\0')
+	{
+		s++;
+	}
+	while (src[j] != '\0')
+	{
+		if (i < size - 1)
+			dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (i);
 }
 
-int main()
+/*int main() 
 {
-    
-}
+	char str1[9] = "aa";
+	char str2[26] = "";
+	char str3[9] = "aa";
+	char str4[26] = "";
+	unsigned int result = ft_strlcat(str2, str1, 4);
+    printf("%d\n%s\n", result, str2);
+	result = strlcat(str4, str3, 4);
+	printf("%d\n%s\n", result, str4);
+    return 0;
+}*/
