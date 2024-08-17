80cd47d26da4805d4a30736e01f2fb8a391cc583
Camila P. Costa <126345439+kamipc@users.noreply.github.com>
2024-08-17 18:21:16 +0100

    fix

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -19,22 +19,25 @@ void	ft_putnbr(int nb);
 
 void	ft_putnbr(int nb)
 {
-	if (nb < 0)
+	long int	n;
+
+	n = nb;
+	if (n < 0)
 	{
 		ft_putchar('-');
-		nb *= -1;
+		n *= -1;
 	}
-	if (nb > 10)
+	if (n > 9)
 	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
 	}
 	else
-		ft_putchar(nb + '0');
+		ft_putchar(n + '0');
 }
 
 /*int main()
 {
-	ft_putnbr(-4222);
+	ft_putnbr(-2147483648);
 	return(0);
 }*/
