718dca409656913a9371bc4a143c71c8248f556d
Camila P. Costa <126345439+kamipc@users.noreply.github.com>
2024-08-18 09:54:44 +0100

    Add files via upload

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user <user@student.42.fr>                  #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-08-18 08:27:16 by user              #+#    #+#             */
+/*   Updated: 2024-08-18 08:27:16 by user             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char a)
+{
+	write (1, &a, 1);
+}
+
+void	ft_print_comb(void)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	while (i <= 7)
+	{
+		j = i + 1;
+		while (j <= 8)
+		{
+			k = j + 1;
+			while (k <= 9)
+			{
+				ft_putchar(i + '0');
+				ft_putchar(j + '0');
+				ft_putchar(k + '0');
+				if (i != 7 && j != 8 && k != 9)
+				{
+					ft_putchar(',');
+					ft_putchar(' ');
+				}
+				else
+					ft_putchar('\n');
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int main()
+{
+	ft_print_comb;
+	return;
+}
